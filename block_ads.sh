125d26c43157d0eac30e9b60bc31acbd509ff282
lithio67 <shliu0607@gmail.com>
2024-12-24 11:39:51 -0600

    Update block_ads.sh

--- block_ads.sh
+++ block_ads.sh
@@ -5,9 +5,13 @@ HOSTS_FILE="/etc/hosts"
 
 # Check if the entry already exists
 if ! grep -q "ads.spotify.com" "$HOSTS_FILE"; then
-    # Append the entry to the hosts file
     echo "127.0.0.1 ads.spotify.com" | sudo tee -a "$HOSTS_FILE" > /dev/null
     echo "Entry added to hosts file."
+    sudo killall -HUP mDNSResponder
+elif ! grep -q "ads-fa.spotify.com" "$HOSTS_FILE"; then
+    echo "127.0.0.1 ads-fa.spotify.com" | sudo tee -a "$HOSTS_FILE" > /dev/null
+    echo "Entry added to hosts file."
+    sudo killall -HUP mDNSResponder
 else
     echo "Entry already exists in hosts file."
 fi
